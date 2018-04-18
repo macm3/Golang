b458d893b51ab0b32cfb45aa2fbbff3394f17cc5
Marcela Azevedo <macm3@cin.ufpe.br>
2018-04-18 01:12:55 -0300

    hello world

---
+++ go/src/Hello.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Word!")
+	fmt.Println("CHANGING TO COMPILE AND RUN AT THE SAME TIME")
+	name := "Marcela"
+	lastName := "Azevedo"
+	age := 22
+	//fmt.Println(reflect.TypeOf(name))
+	fmt.Println(name, lastName+".", "I'm", age, "years old")
+	fmt.Println("Write two numbers at the same line so we can add them")
+	var first int
+	var second int
+	fmt.Scan(&first, &second)
+	var result = plus(first, second)
+	fmt.Println("The result is:", result)
+}
+
+func plus(a int, b int) int {
+	return a + b
+}
